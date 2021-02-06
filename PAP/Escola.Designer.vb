8238ba16a9ccaf78c8488c47db324ea892abe0d4
César Guimarães <cesarguimaraes2003@gmail.com>
2021-02-06 01:33:47 +0000

    Add files via upload

---
+++ PAP/Escola.Designer.vb
@@ -0,0 +1,286 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Escola
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Escola))
+        Me.FormSkinEscola = New PAP.FormSkin()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label19 = New System.Windows.Forms.Label()
+        Me.PictureBoxFechar = New System.Windows.Forms.PictureBox()
+        Me.FlatMinimizar = New PAP.FlatMini()
+        Me.PictureBoxContatos = New System.Windows.Forms.PictureBox()
+        Me.LabelContatos = New System.Windows.Forms.Label()
+        Me.PictureBoxInsta = New System.Windows.Forms.PictureBox()
+        Me.PictureBoxFB = New System.Windows.Forms.PictureBox()
+        Me.LabelInsta = New System.Windows.Forms.Label()
+        Me.LabelFB = New System.Windows.Forms.Label()
+        Me.LabelRSociais = New System.Windows.Forms.Label()
+        Me.PictureBoxIcone = New System.Windows.Forms.PictureBox()
+        Me.LabelSite = New System.Windows.Forms.Label()
+        Me.PictureBoxSite = New System.Windows.Forms.PictureBox()
+        Me.FormSkinEscola.SuspendLayout()
+        CType(Me.PictureBoxFechar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxContatos, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxInsta, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxFB, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxIcone, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBoxSite, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'FormSkinEscola
+        '
+        Me.FormSkinEscola.BackColor = System.Drawing.Color.White
+        Me.FormSkinEscola.BaseColor = System.Drawing.SystemColors.Control
+        Me.FormSkinEscola.BorderColor = System.Drawing.Color.Black
+        Me.FormSkinEscola.Controls.Add(Me.Label1)
+        Me.FormSkinEscola.Controls.Add(Me.Label19)
+        Me.FormSkinEscola.Controls.Add(Me.PictureBoxFechar)
+        Me.FormSkinEscola.Controls.Add(Me.FlatMinimizar)
+        Me.FormSkinEscola.Controls.Add(Me.PictureBoxContatos)
+        Me.FormSkinEscola.Controls.Add(Me.LabelContatos)
+        Me.FormSkinEscola.Controls.Add(Me.PictureBoxInsta)
+        Me.FormSkinEscola.Controls.Add(Me.PictureBoxFB)
+        Me.FormSkinEscola.Controls.Add(Me.LabelInsta)
+        Me.FormSkinEscola.Controls.Add(Me.LabelFB)
+        Me.FormSkinEscola.Controls.Add(Me.LabelRSociais)
+        Me.FormSkinEscola.Controls.Add(Me.PictureBoxIcone)
+        Me.FormSkinEscola.Controls.Add(Me.LabelSite)
+        Me.FormSkinEscola.Controls.Add(Me.PictureBoxSite)
+        Me.FormSkinEscola.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FormSkinEscola.FlatColor = System.Drawing.Color.FromArgb(CType(CType(35, Byte), Integer), CType(CType(168, Byte), Integer), CType(CType(109, Byte), Integer))
+        Me.FormSkinEscola.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.FormSkinEscola.HeaderColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FormSkinEscola.HeaderMaximize = False
+        Me.FormSkinEscola.Location = New System.Drawing.Point(0, 0)
+        Me.FormSkinEscola.Name = "FormSkinEscola"
+        Me.FormSkinEscola.Size = New System.Drawing.Size(361, 422)
+        Me.FormSkinEscola.TabIndex = 0
+        Me.FormSkinEscola.Text = "              Escola"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(-9, 277)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(376, 17)
+        Me.Label1.TabIndex = 49
+        Me.Label1.Text = "════════════☆═════════════════════════════☆═════════════════"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label19
+        '
+        Me.Label19.AutoSize = True
+        Me.Label19.BackColor = System.Drawing.Color.Transparent
+        Me.Label19.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label19.ForeColor = System.Drawing.Color.Black
+        Me.Label19.Location = New System.Drawing.Point(-8, 156)
+        Me.Label19.Name = "Label19"
+        Me.Label19.Size = New System.Drawing.Size(376, 17)
+        Me.Label19.TabIndex = 48
+        Me.Label19.Text = "════════════☆═════════════════════════════☆═════════════════"
+        Me.Label19.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'PictureBoxFechar
+        '
+        Me.PictureBoxFechar.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBoxFechar.Image = CType(resources.GetObject("PictureBoxFechar.Image"), System.Drawing.Image)
+        Me.PictureBoxFechar.Location = New System.Drawing.Point(331, 17)
+        Me.PictureBoxFechar.Name = "PictureBoxFechar"
+        Me.PictureBoxFechar.Size = New System.Drawing.Size(17, 18)
+        Me.PictureBoxFechar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBoxFechar.TabIndex = 35
+        Me.PictureBoxFechar.TabStop = False
+        '
+        'FlatMinimizar
+        '
+        Me.FlatMinimizar.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.FlatMinimizar.BackColor = System.Drawing.Color.White
+        Me.FlatMinimizar.BaseColor = System.Drawing.Color.FromArgb(CType(CType(45, Byte), Integer), CType(CType(47, Byte), Integer), CType(CType(49, Byte), Integer))
+        Me.FlatMinimizar.Font = New System.Drawing.Font("Marlett", 12.0!)
+        Me.FlatMinimizar.Location = New System.Drawing.Point(307, 17)
+        Me.FlatMinimizar.Name = "FlatMinimizar"
+        Me.FlatMinimizar.Size = New System.Drawing.Size(18, 18)
+        Me.FlatMinimizar.TabIndex = 34
+        Me.FlatMinimizar.Text = "FlatMinimizar"
+        Me.FlatMinimizar.TextColor = System.Drawing.Color.FromArgb(CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer), CType(CType(243, Byte), Integer))
+        '
+        'PictureBoxContatos
+        '
+        Me.PictureBoxContatos.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBoxContatos.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBoxContatos.Image = CType(resources.GetObject("PictureBoxContatos.Image"), System.Drawing.Image)
+        Me.PictureBoxContatos.Location = New System.Drawing.Point(155, 299)
+        Me.PictureBoxContatos.Name = "PictureBoxContatos"
+        Me.PictureBoxContatos.Size = New System.Drawing.Size(199, 112)
+        Me.PictureBoxContatos.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBoxContatos.TabIndex = 17
+        Me.PictureBoxContatos.TabStop = False
+        '
+        'LabelContatos
+        '
+        Me.LabelContatos.AutoSize = True
+        Me.LabelContatos.BackColor = System.Drawing.Color.Transparent
+        Me.LabelContatos.Font = New System.Drawing.Font("Segoe UI", 14.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle))
+        Me.LabelContatos.ForeColor = System.Drawing.Color.Black
+        Me.LabelContatos.Location = New System.Drawing.Point(28, 343)
+        Me.LabelContatos.Name = "LabelContatos"
+        Me.LabelContatos.Size = New System.Drawing.Size(92, 25)
+        Me.LabelContatos.TabIndex = 16
+        Me.LabelContatos.Text = "Contatos"
+        '
+        'PictureBoxInsta
+        '
+        Me.PictureBoxInsta.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBoxInsta.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBoxInsta.Image = CType(resources.GetObject("PictureBoxInsta.Image"), System.Drawing.Image)
+        Me.PictureBoxInsta.Location = New System.Drawing.Point(258, 191)
+        Me.PictureBoxInsta.Name = "PictureBoxInsta"
+        Me.PictureBoxInsta.Size = New System.Drawing.Size(91, 77)
+        Me.PictureBoxInsta.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBoxInsta.TabIndex = 15
+        Me.PictureBoxInsta.TabStop = False
+        '
+        'PictureBoxFB
+        '
+        Me.PictureBoxFB.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBoxFB.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBoxFB.Image = CType(resources.GetObject("PictureBoxFB.Image"), System.Drawing.Image)
+        Me.PictureBoxFB.Location = New System.Drawing.Point(164, 191)
+        Me.PictureBoxFB.Name = "PictureBoxFB"
+        Me.PictureBoxFB.Size = New System.Drawing.Size(91, 77)
+        Me.PictureBoxFB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBoxFB.TabIndex = 14
+        Me.PictureBoxFB.TabStop = False
+        '
+        'LabelInsta
+        '
+        Me.LabelInsta.AutoSize = True
+        Me.LabelInsta.BackColor = System.Drawing.Color.Transparent
+        Me.LabelInsta.Font = New System.Drawing.Font("Segoe UI", 13.0!)
+        Me.LabelInsta.ForeColor = System.Drawing.SystemColors.Control
+        Me.LabelInsta.Location = New System.Drawing.Point(257, 172)
+        Me.LabelInsta.Name = "LabelInsta"
+        Me.LabelInsta.Size = New System.Drawing.Size(92, 25)
+        Me.LabelInsta.TabIndex = 13
+        Me.LabelInsta.Text = "Instagram"
+        '
+        'LabelFB
+        '
+        Me.LabelFB.AutoSize = True
+        Me.LabelFB.BackColor = System.Drawing.Color.Transparent
+        Me.LabelFB.Font = New System.Drawing.Font("Segoe UI", 13.0!)
+        Me.LabelFB.ForeColor = System.Drawing.SystemColors.Control
+        Me.LabelFB.Location = New System.Drawing.Point(169, 172)
+        Me.LabelFB.Name = "LabelFB"
+        Me.LabelFB.Size = New System.Drawing.Size(88, 25)
+        Me.LabelFB.TabIndex = 12
+        Me.LabelFB.Text = "Facebook"
+        '
+        'LabelRSociais
+        '
+        Me.LabelRSociais.AutoSize = True
+        Me.LabelRSociais.BackColor = System.Drawing.Color.Transparent
+        Me.LabelRSociais.Font = New System.Drawing.Font("Segoe UI", 14.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle))
+        Me.LabelRSociais.ForeColor = System.Drawing.Color.Black
+        Me.LabelRSociais.Location = New System.Drawing.Point(12, 214)
+        Me.LabelRSociais.Name = "LabelRSociais"
+        Me.LabelRSociais.Size = New System.Drawing.Size(129, 25)
+        Me.LabelRSociais.TabIndex = 11
+        Me.LabelRSociais.Text = "Redes Sociais"
+        '
+        'PictureBoxIcone
+        '
+        Me.PictureBoxIcone.Image = CType(resources.GetObject("PictureBoxIcone.Image"), System.Drawing.Image)
+        Me.PictureBoxIcone.Location = New System.Drawing.Point(27, 3)
+        Me.PictureBoxIcone.Name = "PictureBoxIcone"
+        Me.PictureBoxIcone.Size = New System.Drawing.Size(57, 45)
+        Me.PictureBoxIcone.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBoxIcone.TabIndex = 8
+        Me.PictureBoxIcone.TabStop = False
+        '
+        'LabelSite
+        '
+        Me.LabelSite.AutoSize = True
+        Me.LabelSite.BackColor = System.Drawing.Color.Transparent
+        Me.LabelSite.Font = New System.Drawing.Font("Segoe UI", 14.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle))
+        Me.LabelSite.ForeColor = System.Drawing.Color.Black
+        Me.LabelSite.Location = New System.Drawing.Point(38, 96)
+        Me.LabelSite.Name = "LabelSite"
+        Me.LabelSite.Size = New System.Drawing.Size(82, 25)
+        Me.LabelSite.TabIndex = 7
+        Me.LabelSite.Text = "Website"
+        '
+        'PictureBoxSite
+        '
+        Me.PictureBoxSite.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBoxSite.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBoxSite.Image = CType(resources.GetObject("PictureBoxSite.Image"), System.Drawing.Image)
+        Me.PictureBoxSite.Location = New System.Drawing.Point(196, 63)
+        Me.PictureBoxSite.Name = "PictureBoxSite"
+        Me.PictureBoxSite.Size = New System.Drawing.Size(120, 90)
+        Me.PictureBoxSite.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBoxSite.TabIndex = 6
+        Me.PictureBoxSite.TabStop = False
+        '
+        'Escola
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(361, 422)
+        Me.Controls.Add(Me.FormSkinEscola)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Escola"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Escola"
+        Me.TransparencyKey = System.Drawing.Color.Fuchsia
+        Me.FormSkinEscola.ResumeLayout(False)
+        Me.FormSkinEscola.PerformLayout()
+        CType(Me.PictureBoxFechar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxContatos, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxInsta, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxFB, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxIcone, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBoxSite, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FormSkinEscola As FormSkin
+    Friend WithEvents LabelSite As Label
+    Friend WithEvents PictureBoxSite As PictureBox
+    Friend WithEvents PictureBoxIcone As PictureBox
+    Friend WithEvents PictureBoxInsta As PictureBox
+    Friend WithEvents PictureBoxFB As PictureBox
+    Friend WithEvents LabelInsta As Label
+    Friend WithEvents LabelFB As Label
+    Friend WithEvents LabelRSociais As Label
+    Friend WithEvents PictureBoxContatos As PictureBox
+    Friend WithEvents LabelContatos As Label
+    Friend WithEvents PictureBoxFechar As PictureBox
+    Friend WithEvents FlatMinimizar As FlatMini
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label19 As Label
+End Class
---
+++ PAP/InformacoesDiagnostico.vb
@@ -0,0 +1,52 @@
+﻿Public Class InformacoesDiagnostico
+    Private Sub InformacoesDiagnostico_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If System.Environment.Is64BitOperatingSystem = True Then
+            LabelAr.Text = "x64"
+        Else
+            LabelAr.Text = "x32"
+        End If
+    End Sub
+    Private Sub ButtonSystemInfo_Click(sender As Object, e As EventArgs) Handles ButtonSystemInfo.Click
+        Try
+            Process.Start("\Programas\Diagnostico\systeminfo.bat")
+        Catch ex As Exception
+            MsgBox("Acesso negado pelo utilizador. Consulte o ficheiro Ajuda.txt para ver se o nome da pasta/ficheiro foi alterado. Localização: \Programas\Diagnostico\systeminfo.bat")
+        End Try
+    End Sub
+    Private Sub ButtonIPConfig_Click(sender As Object, e As EventArgs) Handles ButtonIPConfig.Click
+        Try
+            Process.Start("\Programas\Diagnostico\ipconfig.bat")
+        Catch ex As Exception
+            MsgBox("Acesso negado pelo utilizador. Consulte o ficheiro Ajuda.txt para ver se o nome da pasta/ficheiro foi alterado. Localização: \Programas\Diagnostico\ipconfig.bat")
+        End Try
+    End Sub
+    Private Sub ButtonIPConfigAll_Click(sender As Object, e As EventArgs) Handles ButtonIPConfigAll.Click
+        Try
+            Process.Start("\Programas\Diagnostico\ipconfigall.bat")
+        Catch ex As Exception
+            MsgBox("Acesso negado pelo utilizador. Consulte o ficheiro Ajuda.txt para ver se o nome da pasta/ficheiro foi alterado. Localização: \Programas\Diagnostico\ipconfigall.bat")
+        End Try
+    End Sub
+    Private Sub PictureBoxHWINFO_Click_1(sender As Object, e As EventArgs) Handles PictureBoxHWINFO.Click
+        Try
+            If System.Environment.Is64BitOperatingSystem = True Then
+                Process.Start("\Programas\Diagnostico\HWINFO64.exe")
+            Else
+                Process.Start("\Programas\Diagnostico\HWINFO32.exe")
+            End If
+        Catch ex As Exception
+            MsgBox("Acesso negado pelo utilizador. Consulte o ficheiro Ajuda.txt para ver se o nome da pasta/ficheiro foi alterado. Localização: \Programas\Diagnostico\HWINFO32.exe ou HWINFO64.exe")
+        End Try
+    End Sub
+    Private Sub PictureBoxCPUZ_Click(sender As Object, e As EventArgs) Handles PictureBoxCPUZ.Click
+        Try
+            Process.Start("\Programas\Diagnostico\CPU-Z\cpuz.exe")
+        Catch ex As Exception
+            MsgBox("Acesso negado pelo utilizador. Consulte o ficheiro Ajuda.txt para ver se o nome da pasta/ficheiro foi alterado. Localização: \Programas\Diagnostico\CPU-Z\cpuz.exe")
+        End Try
+    End Sub
+    'Botão de fechar
+    Private Sub PictureBoxFechar_Click(sender As Object, e As EventArgs) Handles PictureBoxFechar.Click
+        Me.Close()
+    End Sub
+End Class
